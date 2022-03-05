d8620eb49242c804a6770054dc587de27b0032a8
Alexander Roshchin <alexander.roshchin85@gmail.com>
2022-03-05 22:04:22 +0200

    Added Sidebar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ import logo from '../../assets/images/Maximus-Logo.png';
 
 const Navbar = () => {
   return (
-    <Box minH="480px">
+    <Box minH="84px">
       <Box as="header" bg={mode('white', 'gray.800')} borderBottomWidth="1px">
         <Box maxW="7xl" mx="auto" py="4" px={{base: '6', md: '8'}}>
           <Flex as="nav" justify="space-between">
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,43 @@
+import { Divider, Flex, Spacer, Stack } from '@chakra-ui/react'
+import * as React from 'react'
+import {
+  FaRegChartBar,
+  FaFire,
+  FaUser,
+  FaRegIdCard,
+  FaNetworkWired
+} from 'react-icons/fa'
+import { NavLink } from './NavLink'
+import { UserProfile } from './UserProfile'
+import { useColorModeValue as mode } from '@chakra-ui/react';
+
+export const Sidebar: React.FC = () => {
+  return (
+    <Flex
+      height="calc(100vh - 84px)"
+      width={{ base: 'full', sm: 'xs' }}
+      direction="column"
+      bg={mode('white', 'gray.800')}
+      color="white"
+      px={6}
+      py={8}
+    >
+      <Stack spacing={6}>
+        <Stack>
+          <NavLink label="Clients" icon={FaUser} isActive />
+          <NavLink label="Memberships" icon={FaRegIdCard} />
+          <NavLink label="Trainers" icon={FaNetworkWired} />
+          <NavLink label="Workouts" icon={FaFire} />
+          <NavLink label="Statistics" icon={FaRegChartBar} />
+        </Stack>
+        <Divider borderColor="whiteAlpha.400" />
+      </Stack>
+      <Spacer />
+      <UserProfile
+        name="Cindy Winston"
+        image="https://images.unsplash.com/photo-1521296797187-726205347ca9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fGxhZHklMjBzbWlsaW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        email="cindy@example.com"
+      />
+    </Flex>
+  )
+}
